4b5a95e10c5637817e114376118a224a3a9d5a95
StevenSteev <201804788h@utea.edu.pe>
2022-11-29 18:09:23 -0500

    avance vid16 perdon por no comitear anteriores

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pokemon/pages/home_page.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}): super(key: key);
-  
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "TaskDBApp",
-      debugShowCheckedModeBanner: false ,
-      home : HomePage(),
+      title: "TaskBD",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
